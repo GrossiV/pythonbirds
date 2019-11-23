02853e8fa4ccced919d7915bceecfb26e94d5c18
Victor <victoraugustogrossi@gmail.com>
2019-11-23 11:57:49 -0300

    atributo complexo

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,11 +1,16 @@
 class Pessoa:
-    def __init__(self, nome=None):
+    def __init__(self, *filhos, nome=None):
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'{self.nome} {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Victor')
-    print(p.cumprimentar())
+    frodo = Pessoa(nome='Frodo')
+    victor = Pessoa(frodo, nome='Victor')
+    print(victor.cumprimentar())
+
+    for filho in victor.filhos:
+        print(filho.nome)
