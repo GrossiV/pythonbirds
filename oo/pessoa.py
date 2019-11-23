b9d762409ac124b2d7db59a3a06256ad95c7968d
Victor <victoraugustogrossi@gmail.com>
2019-11-23 11:38:56 -0300

    Criação de atributo da instancia

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,11 @@
 class Pessoa:
+    def __init__(self, nome=None):
+        self.nome = nome
+
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        return f'{self.nome} {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Victor')
     print(p.cumprimentar())
